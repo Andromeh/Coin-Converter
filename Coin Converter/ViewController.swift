5d1f53e07e0175b7cdf1533d20b5a669c23a5b7c
Andromeh <Andromeh@mail.ru>
2022-03-04 14:20:04 +0300

    Initial Commit

---
+++ Coin Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin Converter
+//
+//  Created by Белозеров Константин on 04.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
